44a86556aeb88f54c6753adc110dd1282a1960ef
Reymart Manzano <2163903@slu.edu.ph>
2018-04-10 02:28:13 +0800

    fixed PWA website

--- PWA2/src/script/script.js
+++ PWA2/src/script/script.js
@@ -1,4 +1,31 @@
-/*window.onload = function(){
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+window.onload = function(){
 	            var canvas = document.getElementById("mapCanvas");
 	            var ctx = canvas.getContext("2d");
 	            
@@ -6,7 +33,7 @@
 	            ctx.drawImage(pic, 0,0);
 }
 
-/**(function() {
+(function() {
   var boxes = document.getElementsByClassName("image-box");
   
   Array.prototype.forEach.call(boxes, function(box) {
@@ -26,7 +53,7 @@
       btn.classList.remove("show");
     });
   });
-})();*/
+})();
 
 function openNav(){
 	document.getElementById("sidenavbar").style.width = "250px";
