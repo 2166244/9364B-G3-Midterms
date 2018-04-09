2b019673d435344d08152e7a3d02a27f08112d95
2165522 <2165522@slu.edu.ph>
2018-04-09 22:56:15 +0800

    organized files

--- app.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-    var canvas = document.getElementById("mapCanvas");
-    var ctx = canvas.getContext("2d");
-    var pic = document.getElementById("baguioMap");
-    ctx.drawImage(pic, 0,0);
-}
