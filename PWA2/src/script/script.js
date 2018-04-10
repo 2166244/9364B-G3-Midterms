9375a6831697bba10ca1db96c47474b982d7c637
2165522 <2165522@slu.edu.ph>
2018-04-10 23:02:29 +0800

    update

--- PWA2/src/script/script.js
+++ PWA2/src/script/script.js
@@ -1,67 +1,77 @@
+function onClick(el) {
+    link_id = el.id;
+
+    switch(link_id) {
+        case "panagbenga_park_link":
+            q = 0;
+            break;
+        case "sunshine_park_link":
+            q = 1;
+            break;
+    }
+
+    console.log(q);
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
 }
 
-window.onload = function(){
-	            var canvas = document.getElementById("mapCanvas");
-	            var ctx = canvas.getContext("2d");
-	            
-	            var pic = document.getElementById("baguioMap");
-	            ctx.drawImage(pic, 0,0);
-}
 
-(function() {
-  var boxes = document.getElementsByClassName("image-box");
-  
-  Array.prototype.forEach.call(boxes, function(box) {
-    var detail = box.getElementsByClassName("image-detail")[0];
-    var img = box.getElementsByTagName("img")[0];
-    var btn = box.getElementsByClassName("button")[0];
-    
-    box.addEventListener("mouseover", function() {
-      detail.classList.add("hide");
-      img.classList.add("detail-hover-state");
-      btn.classList.add("show");
+function() {
+    var boxes = document.getElementsByClassName("image-box");
+
+    Array.prototype.forEach.call(boxes, function(box) {
+        var detail = box.getElementsByClassName("image-detail")[0];
+        var img = box.getElementsByTagName("img")[0];
+        var btn = box.getElementsByClassName("button")[0];
+
+        box.addEventListener("mouseover", function() {
+            detail.classList.add("hide");
+            img.classList.add("detail-hover-state");
+            btn.classList.add("show");
+        });
+
+        box.addEventListener("mouseout", function() {
+            detail.classList.remove("hide");
+            img.classList.remove("detail-hover-state");
+            btn.classList.remove("show");
+        });
     });
-    
-    box.addEventListener("mouseout", function() {
-      detail.classList.remove("hide");
-      img.classList.remove("detail-hover-state");
-      btn.classList.remove("show");
-    });
-  });
 })();
 
 function openNav(){
-	document.getElementById("sidenavbar").style.width = "250px";
+    document.getElementById("sidenavbar").style.width = "250px";
 }
 
 function closeNav(){
-	document.getElementById("sidenavbar").style.width = "0px";
+    document.getElementById("sidenavbar").style.width = "0px";
 }
+
+
 
 
 
